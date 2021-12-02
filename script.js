255cc7d8614da246d3404504a15bf97394c90130
doctornivag <doctornivag@penguin.com>
2021-12-02 09:51:04 -0800

    Made Conditionals and Operations

---
+++ script.js
@@ -0,0 +1,7 @@
+if (10 * 10 === 100) {
+    console.log('You suppuh Smart! 10 X 10 is 100! wooo!');
+}
+
+if (4 && 5) {
+    console.log('There is a very long and complicated problem that proves 4=5, look it up on Youtube. Technically, by that logic, 2+2=5, think about it.')
+}
